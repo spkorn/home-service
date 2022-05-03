5500be749d2ed0feb3a27c01e3bd8ddb9432f6c9
Supakorn Meelarp <s_meelarp@hotmail.com>
2022-05-03 17:55:14 +0700

    Delete imageIndex.js

--- client/src/HomePagePhoto/imageIndex.js
+++
@@ -1,9 +0,0 @@
-import jobNoticeHouseLogo from "./homepage-job-notice-logo.png";
-import jobNoticeWorker from "./homepage-job-notice-picture.png";
-
-
-const image = {
-    jobNoticeHouseLogo, jobNoticeWorker
-}
-
-export default image;
