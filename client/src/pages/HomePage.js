c1b1ad2f8c2c3c93b485a4a4a0335476e58f2e22
Parn <pause.forever@gmail.com>
2022-05-03 16:38:50 +0700

    feature: add job notice in homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,22 +1,11 @@
-import "../App.css";
-import { css } from "@emotion/react";
+import HomePageJobNotice from "../components/HomePageJobNotice";
 
 function HomePage() {
   return (
-    <div className="home-page">
-      <div className="App">
-        <h1
-          className="text-blue900"
-          // css={css`
-          //   font-family: prompt;
-          // `}
-        >
-          Hello WORLD บริการของเรา
-        </h1>
-        <button className="bg-blue300">PRAYUTH</button>
-      </div>
+    <div>
+      <HomePageJobNotice />
     </div>
-  );
+  )
 }
 
 export default HomePage;
