ff196084791f320a18874e6d9d5c37b0b6b27fa1
Parn <pause.forever@gmail.com>
2022-05-05 10:57:01 +0700

    create login api

--- server/apps/services.js
+++ server/apps/services.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { pool } from "../utils/db.js"
+import { protect } from "../middlewares/protects.js";
 
 const serviceRouter = Router();
+serviceRouter.use(protect);
 
