18b257c6ce5e4a0d76b163fb2736dc472b6678b0
spkorn <s_meelarp@hotmail.com>
2022-05-06 10:42:25 +0700

    create: service page job notice

---
+++ client/src/pages/ServicePage.js
@@ -0,0 +1,11 @@
+import ServicePageJobNotice from "../components/ServicePage/ServicesPageJobNotice";
+
+function ServicePage() {
+  return (
+    <div className="service-page">
+      <ServicePageJobNotice />
+    </div>
+  );
+}
+
+export default ServicePage;
--- client/src/pages/UnauthenticatedApp.js
+++ client/src/pages/UnauthenticatedApp.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
+import ServicePage from "./ServicePage";
 import "../App.css";
 
 function UnauthenticatedApp() {
@@ -9,6 +10,7 @@ function UnauthenticatedApp() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/service" element={<ServicePage />} />
         <Route path="/login" element={<LoginPage />} /> 
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
