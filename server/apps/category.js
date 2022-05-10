62aa8d2679f65db7e4b531ab3f407d59b5a10c57
spkorn <s_meelarp@hotmail.com>
2022-05-10 11:33:27 +0700

    feat: create create-category-api

---
+++ server/apps/category.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { pool } from "../utils/db.js";
+import { protect } from "../middlewares/protects.js";
+
+const categoryRouter = Router();
+//categoryRouter.use(protect);
+
+categoryRouter.post("/", async (req, res) => {
+  const newCategory = {
+    category_name: req.body.category_name,
+    category_created_date: new Date(),
+    category_edited_date: new Date(),
+  };
+
+  await pool.query(
+    `insert into category (category_name, category_created_date, category_edited_date) 
+    values ($1, $2, $3)`,
+    [
+      newCategory.category_name,
+      newCategory.category_created_date,
+      newCategory.category_edited_date,
+    ]
+  );
+
+  return res.json({
+    message: "New category has been created successfully.",
+  });
+});
+
+export default categoryRouter;
--- server/homeServices.sql
+++ server/homeServices.sql
@@ -1,15 +1,4 @@
-DROP TABLE IF EXISTS users, service CASCADE; 
-
-create table service (
-service_id int primary key generated always as identity,
-service_name text,
-category text not null,
-price_range_estimate decimal (7,2) not null,
-service_photo text not null,
-created_date timestamptz not null,
-edited_date timestamptz not null,
-user_id int
-);
+DROP TABLE IF EXISTS users, service, category CASCADE; 
 
 create table users (
 user_id int primary key generated always as identity,
@@ -20,16 +9,26 @@ password varchar (100) not null,
 role text not null
 );
 
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งเครื่องดูดควัน', 'บริการห้องครัว', 5805.22, 'libero nam dui proin leo odio porttitor id consequat in consequat ut nulla sed accumsan felis ut at dolor quis odio consequat varius integer ac leo', '2021-08-15T04:00:55Z', '2021-10-07T11:42:31Z', 1);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ล้างแอร์', 'บริการทั่วไป', 3406.16, 'aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus tortor', '2021-07-11T13:19:59Z', '2022-03-05T18:16:13Z', 2);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ซ่อมแอร์', 'บริการทั่วไป', 2748.54, 'luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis', '2022-04-06T20:54:35Z', '2022-02-04T00:55:15Z', 3);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ซ่อมตู้ครัว', 'บริการห้องครัว', 9951.95, 'quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a nibh in quis', '2021-12-13T23:58:56Z', '2021-10-29T16:02:29Z', 4);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ซ่อมเครื่องซักผ้า', 'บริการทั่วไป', 1389.99, 'convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam', '2021-08-31T03:24:17Z', '2021-07-17T00:17:44Z', 5);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งชักโครก', 'บริการห้องน้ำ', 1551.76, 'erat id mauris vulputate elementum nullam varius nulla facilisi cras non', '2021-09-16T17:06:46Z', '2021-07-04T14:13:47Z', 6);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งเตาแก๊ส', 'บริการห้องครัว', 1340.75, 'purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum', '2021-06-14T20:09:15Z', '2021-07-03T12:23:32Z', 7);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งเครื่องทำน้ำอุ่น', 'บริการห้องน้ำ', 5039.39, 'convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in', '2021-11-29T17:24:14Z', '2022-05-06T13:27:44Z', 8);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งชักโครก', 'บริการห้องน้ำ', 2759.59, 'lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi', '2021-12-15T08:16:47Z', '2021-07-18T04:15:58Z', 9);
-insert into service (service_name, category, price_range_estimate, service_photo, created_date, edited_date, user_id) values ('ติดตั้งเตาแก๊ส', 'บริการห้องครัว', 3620.08, 'interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu', '2022-03-16T23:24:20Z', '2021-12-10T07:02:21Z', 10);
+create table category (
+category_id int primary key generated always as identity,
+category_name text not null,
+category_created_date timestamptz not null,
+category_edited_date timestamptz not null
+
+);
+
+create table service (
+service_id int primary key generated always as identity,
+user_id int references users(user_id) on delete cascade,
+category_id int references category(category_id) on delete cascade,
+service_name text,
+price_range_estimate decimal (7,2) not null,
+service_photo text not null,
+service_created_date timestamptz not null,
+service_edited_date timestamptz not null
+);
+
+
 
 insert into users (name, phoneNumber, email, password, role) values ('Stephan Edmeades', '0879316420', 'sedmeades0@goodreads.com', 'iP4EAxpCF4i', 'customer');
 insert into users (name, phoneNumber, email, password, role) values ('Fredia Nijs', '0342821343', 'fnijs1@twitpic.com', 'Yuapex', 'customer');
@@ -39,3 +38,27 @@ insert into users (name, phoneNumber, email, password, role) values ('Alexi Swor
 insert into users (name, phoneNumber, email, password, role) values ('Bernetta Goatman', '0885067947', 'bgoatman5@creativecommons.org', 'dlCSIq78BU', 'customer');
 insert into users (name, phoneNumber, email, password, role) values ('Burnaby Trouel', '0607150189', 'btrouel6@microsoft.com', 'pwRWWe5rC3x', 'admin');
 insert into users (name, phoneNumber, email, password, role) values ('Del Kliement', '0165268368', 'dkliement7@360.cn', 'PX6YMnV', 'admin');
+insert into users (name, phoneNumber, email, password, role) values ('Supakorn Korn', '0165656658', 'kornpupakorn@gmail.com', 'maiborkmaibork', 'admin');
+insert into users (name, phoneNumber, email, password, role) values ('Prayuth Huakuy', '0000222332', 'hahaha@360.cn', 'PArnPards', 'customer');
+
+insert into category (category_name, category_created_date, category_edited_date) values ('บริการห้องครัว', '2021-09-18T02:26:51Z', '2022-02-17T15:15:13Z');
+insert into category (category_name, category_created_date, category_edited_date) values ('บริการทั่วไป', '2022-04-22T12:24:24Z', '2022-04-12T04:17:17Z');
+insert into category (category_name, category_created_date, category_edited_date) values ('บริการห้องน้ำ', '2021-06-07T09:25:13Z', '2021-08-07T23:23:22Z');
+
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (10, 1, 'ติดตั้งเครื่องดูดควัน', 5805.22, 'libero nam dui proin leo odio porttitor id consequat in consequat ut nulla sed accumsan felis ut at dolor quis odio consequat varius integer ac leo', '2021-08-15T04:00:55Z', '2021-10-07T11:42:31Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (9, 2, 'ล้างแอร์', 3406.16, 'aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus tortor', '2021-07-11T13:19:59Z', '2022-03-05T18:16:13Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (8, 2, 'ซ่อมแอร์', 2748.54, 'luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis', '2022-04-06T20:54:35Z', '2022-02-04T00:55:15Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (7, 1, 'ซ่อมตู้ครัว', 9951.95, 'quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a nibh in quis', '2021-12-13T23:58:56Z', '2021-10-29T16:02:29Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (6, 2, 'ซ่อมเครื่องซักผ้า', 1389.99, 'convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam', '2021-08-31T03:24:17Z', '2021-07-17T00:17:44Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (5, 3, 'ติดตั้งชักโครก', 1551.76, 'erat id mauris vulputate elementum nullam varius nulla facilisi cras non', '2021-09-16T17:06:46Z', '2021-07-04T14:13:47Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (4, 1, 'ติดตั้งเตาแก๊ส', 1340.75, 'purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum', '2021-06-14T20:09:15Z', '2021-07-03T12:23:32Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (3, 3, 'ติดตั้งเครื่องทำน้ำอุ่น', 5039.39, 'convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in', '2021-11-29T17:24:14Z', '2022-05-06T13:27:44Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (2, 3, 'ติดตั้งชักโครก', 999.59, 'lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi', '2021-12-15T08:16:47Z', '2021-07-18T04:15:58Z');
+insert into service (user_id, category_id, service_name, price_range_estimate, service_photo, service_created_date, service_edited_date) values (1, 1, 'ติดตั้งเตาแก๊ส', 3620.08, 'interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu', '2022-03-16T23:24:20Z', '2021-12-10T07:02:21Z');
+
+-- select *
+-- from service
+-- inner join category
+-- on category.category_id = service.category_id
+
+
