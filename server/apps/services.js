58b5325c0abaf854f22674776e7635ca160daae8
spkorn <s_meelarp@hotmail.com>
2022-05-26 11:02:34 +0700

    fix: delete sub_service_quantity and total_price from sub_service

--- server/apps/services.js
+++ server/apps/services.js
@@ -9,23 +9,28 @@ const serviceRouter = Router();
 
 const multerUpload = multer({ dest: "uploads/" });
 const servicePhotoUpload = multerUpload.fields([
- { name: "servicePhoto", maxCount: 1 },
+  { name: "servicePhoto", maxCount: 1 },
 ]);
 
 // API route to service listing page
 serviceRouter.get("/", async (req, res) => {
   const keywords = req.query.keywords || "";
   const categoryFilter = req.query.categoryFilter || "";
-  const maxPriceFilter = req.query.maxPriceFilter || Number.MAX_SAFE_INTEGER; 
+  const maxPriceFilter = req.query.maxPriceFilter || Number.MAX_SAFE_INTEGER;
   const minPriceFilter = req.query.minPriceFilter || 0;
   const orderFilter = req.query.orderFilter || "";
 
   let query = "";
   let values = [];
 
- if (keywords && categoryFilter && maxPriceFilter && minPriceFilter && (orderFilter === 'asc')) {
-  
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+  if (
+    keywords &&
+    categoryFilter &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "asc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -36,12 +41,16 @@ serviceRouter.get("/", async (req, res) => {
       where service.service_name ilike '%'||$1||'%' and category.category_name = $2 and
       (sub_service.price_per_unit <= $3) and (sub_service.price_per_unit >= $4)
       group by service.service_id, category.category_name
-      order by service.service_name asc`); // '%' || tag_name || '%' can search anything in keywords
-      values = [keywords, categoryFilter, maxPriceFilter, minPriceFilter];
-    
-    } else if (keywords && categoryFilter && maxPriceFilter && minPriceFilter && (orderFilter === 'desc')) {
-  
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name asc`; // '%' || tag_name || '%' can search anything in keywords
+    values = [keywords, categoryFilter, maxPriceFilter, minPriceFilter];
+  } else if (
+    keywords &&
+    categoryFilter &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "desc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -52,12 +61,15 @@ serviceRouter.get("/", async (req, res) => {
       where service.service_name ilike '%'||$1||'%' and category.category_name = $2 and
       (sub_service.price_per_unit <= $3) and (sub_service.price_per_unit >= $4)
       group by service.service_id, category.category_name
-      order by service.service_name desc`); // '%' || tag_name || '%' can search anything in keywords
-      values = [keywords, categoryFilter, maxPriceFilter, minPriceFilter];
-
-    } else if (keywords && maxPriceFilter && minPriceFilter && (orderFilter === 'asc')) {
-      
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name desc`; // '%' || tag_name || '%' can search anything in keywords
+    values = [keywords, categoryFilter, maxPriceFilter, minPriceFilter];
+  } else if (
+    keywords &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "asc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -68,12 +80,15 @@ serviceRouter.get("/", async (req, res) => {
 	    where service.service_name ilike '%'||$1||'%' and (sub_service.price_per_unit <= $2) 
       and (sub_service.price_per_unit >= $3)
       group by service.service_id, category.category_name
-      order by service.service_name asc`);
-      values = [keywords, maxPriceFilter, minPriceFilter];
-  
-    } else if (keywords && maxPriceFilter && minPriceFilter && (orderFilter === 'desc')) {
-      
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name asc`;
+    values = [keywords, maxPriceFilter, minPriceFilter];
+  } else if (
+    keywords &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "desc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -84,12 +99,15 @@ serviceRouter.get("/", async (req, res) => {
 	    where service.service_name ilike '%'||$1||'%' and (sub_service.price_per_unit <= $2) 
       and (sub_service.price_per_unit >= $3)
       group by service.service_id, category.category_name
-      order by service.service_name desc`);
-      values = [keywords, maxPriceFilter, minPriceFilter];
-
-    } else if (categoryFilter && maxPriceFilter && minPriceFilter && (orderFilter === 'asc')) {
-  
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name desc`;
+    values = [keywords, maxPriceFilter, minPriceFilter];
+  } else if (
+    categoryFilter &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "asc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -100,12 +118,15 @@ serviceRouter.get("/", async (req, res) => {
 	    where category.category_name = $1 and (sub_service.price_per_unit <= $2) 
       and (sub_service.price_per_unit >= $3)
       group by service.service_id, category.category_name
-      order by service.service_name asc`);
-      values = [categoryFilter, maxPriceFilter, minPriceFilter];
-    
-    } else if (categoryFilter && maxPriceFilter && minPriceFilter && (orderFilter === 'desc')) {
-      
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name asc`;
+    values = [categoryFilter, maxPriceFilter, minPriceFilter];
+  } else if (
+    categoryFilter &&
+    maxPriceFilter &&
+    minPriceFilter &&
+    orderFilter === "desc"
+  ) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -116,12 +137,10 @@ serviceRouter.get("/", async (req, res) => {
 	    where category.category_name = $1 and (sub_service.price_per_unit <= $2) 
       and (sub_service.price_per_unit >= $3)
       group by service.service_id, category.category_name
-      order by service.service_name desc`);
-      values = [categoryFilter, maxPriceFilter, minPriceFilter];
-    
-    } else if (maxPriceFilter && minPriceFilter && (orderFilter === 'asc')) {
-    
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name desc`;
+    values = [categoryFilter, maxPriceFilter, minPriceFilter];
+  } else if (maxPriceFilter && minPriceFilter && orderFilter === "asc") {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -131,12 +150,10 @@ serviceRouter.get("/", async (req, res) => {
       on category.category_id = service.category_id
       where (sub_service.price_per_unit <= $1) and (sub_service.price_per_unit >= $2)
       group by service.service_id, category.category_name
-      order by service.service_name asc`);
-      values = [maxPriceFilter, minPriceFilter];
-   
-    } else if (maxPriceFilter && minPriceFilter && (orderFilter === 'desc')) {
-    
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_name asc`;
+    values = [maxPriceFilter, minPriceFilter];
+  } else if (maxPriceFilter && minPriceFilter && orderFilter === "desc") {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -146,12 +163,10 @@ serviceRouter.get("/", async (req, res) => {
       on category.category_id = service.category_id
       where (sub_service.price_per_unit <= $1) and (sub_service.price_per_unit >= $2)
       group by service.service_id, category.category_name
-      order by service.service_name desc`);
-      values = [maxPriceFilter, minPriceFilter]; 
-   
-    } else if (keywords) {
-
-      (query = `select service.service_id, category.category_name, service.category_id, service.service_name,
+      order by service.service_name desc`;
+    values = [maxPriceFilter, minPriceFilter];
+  } else if (keywords) {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name,
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -161,11 +176,10 @@ serviceRouter.get("/", async (req, res) => {
       on category.category_id = service.category_id
       where service.service_name ilike '%'||$1||'%'
       group by service.service_id, category.category_name
-      order by service.service_id asc`);
-      values = [keywords];
-    
-    } else {
-      query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
+      order by service.service_id asc`;
+    values = [keywords];
+  } else {
+    query = `select service.service_id, category.category_name, service.category_id, service.service_name, 
       service.service_photo, min(sub_service.price_per_unit) as min_price, 
       max(sub_service.price_per_unit) as max_price, service.service_created_date, service.service_edited_date
       from service
@@ -176,12 +190,11 @@ serviceRouter.get("/", async (req, res) => {
       group by service.service_id, category.category_name
       order by service.service_id asc`;
   }
-    const results = await pool.query(query, values);
+  const results = await pool.query(query, values);
 
-    return res.status(200).json({
-      data: results.rows,
-    });
-  
+  return res.status(200).json({
+    data: results.rows,
+  });
 });
 
 //API route to one service item page
@@ -204,7 +217,7 @@ serviceRouter.get("/:id", async (req, res) => {
     order by sub_service.sub_service_id asc`,
     [serviceId]
   );
-  //console.log(result.rows);
+
   return res.json({
     data: result.rows,
   });
@@ -224,7 +237,7 @@ serviceRouter.post("/", servicePhotoUpload, async (req, res) => {
     service_edited_date: new Date(),
   };
   //console.log(JSON.parse(req.body.sub_service))
-  
+
   const servicePhotoUrl = await cloudinaryUpload(req.files);
   newServiceItem["servicePhotos"] = servicePhotoUrl;
 
@@ -239,12 +252,12 @@ serviceRouter.post("/", servicePhotoUpload, async (req, res) => {
       newServiceItem.service_edited_date,
     ]
   );
-  
+
   if (newServiceItem.sub_service) {
     for (let r = 0; r <= newServiceItem.sub_service.length - 1; r++) {
       await pool.query(
-        `insert into sub_service ( service_id, sub_service_name, unit, price_per_unit, sub_service_quantity, total_price)
-    values ((select service_id from service where service_name = $1 ), $2, $3, $4, 0, 0);`,
+        `insert into sub_service ( service_id, sub_service_name, unit, price_per_unit)
+    values ((select service_id from service where service_name = $1 ), $2, $3, $4);`,
         [
           newServiceItem.service_name,
           newServiceItem.sub_service[r].sub_service_name,
@@ -263,32 +276,23 @@ serviceRouter.post("/", servicePhotoUpload, async (req, res) => {
 //API route to update existing service item page
 serviceRouter.put("/:id", async (req, res) => {
   const updateServiceItem = {
-    data: [
-      ...req.body
-    
-      // service_name: req.body.service_name,
-    // category_name: req.body.category_name,
-    // servicePhotos: req.body.servicePhoto,
-    // sub_service_name: req.body.sub_service_name,
-    // unit: req.body.unit,
-    // price_per_unit: req.body.price_per_unit,
-    // service_edited_date: new Date(),
-  ]
+    data: [...req.body],
   };
-  
+
   const serviceId = req.params.id;
   // const servicePhotoUrl = await cloudinaryUpload(req.files);
   // updateServiceItem["servicePhotos"] = servicePhotoUrl;
-  console.log(updateServiceItem)
+
   const subServiceId = [];
 
+  // เก็บ sub_service_id ที่จะทำการ update
   for (let r = 0; r <= updateServiceItem.data.length - 1; r++) {
     subServiceId.push(updateServiceItem.data[r].sub_service_id);
   }
-  
+
   // ใช้ได้แล้ว
   for (let r = 0; r <= updateServiceItem.data.length - 1; r++) {
-   await pool.query(
+    await pool.query(
       `update service
     set service_name=$1,  
     category_id=(select category_id from category where category_name=$2), 
@@ -299,13 +303,14 @@ serviceRouter.put("/:id", async (req, res) => {
         updateServiceItem.data[updateServiceItem.data.length - 1].service_name,
         updateServiceItem.data[updateServiceItem.data.length - 1].category_name,
         // updateServiceItem.data[updateServiceItem.data.length - 1].servicePhotos[0],
-        (updateServiceItem.data[updateServiceItem.data.length - 1]
-          .service_edited_date = new Date()),
+        (updateServiceItem.data[
+          updateServiceItem.data.length - 1
+        ].service_edited_date = new Date()),
         serviceId,
       ]
     );
 
-    // ให้ลบ sub_service_id ทั้งหมดที่ไม่อยู่ใน array subServiceId
+    // ให้ลบ sub_service_id ทั้งหมดที่ไม่อยู่ใน array subServiceId และอยู่ใน service_id นั้นๆ
     await pool.query(
       `delete from sub_service where sub_service_id != all($1) and service_id =$2`,
       [subServiceId, serviceId]
@@ -313,18 +318,19 @@ serviceRouter.put("/:id", async (req, res) => {
 
     if (!updateServiceItem.data[r].sub_service_id) {
       await pool.query(
-      `insert into sub_service ( service_id, sub_service_name, unit, price_per_unit, sub_service_quantity, total_price)
-    values ((select service_id from service where service_name = $1 ), $2, $3, $4, 0, 0);`,
-    [
-      updateServiceItem.data[r].service_name,
-      updateServiceItem.data[r].sub_service_name,
-      updateServiceItem.data[r].unit,
-      updateServiceItem.data[r].price_per_unit,
-    ])
+        `insert into sub_service ( service_id, sub_service_name, unit, price_per_unit)
+    values ((select service_id from service where service_name = $1 ), $2, $3, $4);`,
+        [
+          updateServiceItem.data[r].service_name,
+          updateServiceItem.data[r].sub_service_name,
+          updateServiceItem.data[r].unit,
+          updateServiceItem.data[r].price_per_unit,
+        ]
+      );
     }
 
     await pool.query(
-      `update sub_service set sub_service_name=$1, unit=$2, price_per_unit=$3, sub_service_quantity=0, total_price=0
+      `update sub_service set sub_service_name=$1, unit=$2, price_per_unit=$3
     where sub_service_id=$4`,
       [
         updateServiceItem.data[r].sub_service_name,
@@ -352,4 +358,3 @@ serviceRouter.delete("/:id", async (req, res) => {
 });
 
 export default serviceRouter;
-
