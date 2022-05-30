3f0dd071e23ae7bd962a67e56b8ee49fde5435ac
Kanjanapas <oshin_2544@hotmail.com>
2022-05-30 11:22:33 +0700

    Created card payment complete

--- client/src/CustomerPhoto/imageIndex.js
+++ client/src/CustomerPhoto/imageIndex.js
@@ -1,11 +1,13 @@
 import orderIcon from './icons/order-icon.png'
 import paymentIcon from './icons/payment-icon.png'
 import penIcon from './icons/pen-icon.png'
+import completeIcon from './icons/complete-icon.png'
 
 const image = {
   orderIcon,
   paymentIcon,
-  penIcon
+  penIcon,
+  completeIcon
 }
 
 export default image
