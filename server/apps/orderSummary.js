4d2824a25d337b6a5aea6a234f6f5c79e33e7c78
Parn <pause.forever@gmail.com>
2022-05-30 13:49:37 +0700

    bug fix

--- server/apps/orderSummary.js
+++ server/apps/orderSummary.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { pool } from "..utils/db.js";
+import { pool } from "../utils/db.js";
 import { protect } from "../middlewares/protects.js";
 
 const orderSummaryRouter = Router();
