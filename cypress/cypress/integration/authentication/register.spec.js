86cc6b8f97c5e5df2669e3b8841a343a49959700
Parn <pause.forever@gmail.com>
2022-06-02 09:51:03 +0700

    ได้ปะวะเนี่ย

--- cypress/cypress/integration/authentication/register.spec.js
+++ cypress/cypress/integration/authentication/register.spec.js
@@ -38,3 +38,37 @@ it("user should login successfully", () => {
   cy.get(".btn-primary").click();
   cy.url().should("eq", "http://localhost:3000/");
 });
+
+// edge case #1 - test unsuccessful registration due to incomplete filling
+it("user should not be able to register because of one missing filling", () => {
+  cy.visit("http://localhost:3000/register").viewport(1500, 660);
+  cy.get("#name").type("Chadchart Sittipun");
+  cy.get("#phoneNumber").type("0879316420");
+  cy.get("#email").type("Chadchart@sittipun.com");
+  cy.get("#password").type("Chat888Chard");
+  cy.get(".btn-primary").click();
+  cy.get(".form-actions > .text-red")
+  cy.url().should("eq", "http://localhost:3000/register");
+});
+
+
+// edge case #2 - test unsuccessful registration due to incomplete filling
+it("user should not be able to register", () => {
+  cy.visit("http://localhost:3000/register").viewport(1500, 660);
+  cy.get("#name").type("Chadchart");
+  cy.get("#phoneNumber").type("0879316428768768768700000");
+  cy.get("#email").type("Chadchartsittipun.com");
+  cy.get("#password").type("1234");
+  cy.get("#conditionAndPrivacy").click();
+  cy.get(".btn-primary").click();
+  cy.get(":nth-child(2) > label > .text-xs");
+  cy.get(":nth-child(3) > label > .text-xs");
+  cy.get(":nth-child(4) > label > .text-xs");
+  cy.get(":nth-child(5) > :nth-child(1) > .text-xs");
+  cy.get(".form-actions > .text-red");
+  cy.url().should("eq", "http://localhost:3000/register");
+});
+
+// edge case #3
+
+
